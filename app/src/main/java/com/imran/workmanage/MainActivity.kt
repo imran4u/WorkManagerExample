933840e364b8d6952a067534c11b6b7048c43178
imran <er.imran4u@gmail.com>
2020-06-24 16:24:23 +0530

    Show simple notification from worker

--- app/src/main/java/com/imran/workmanage/MainActivity.kt
+++ app/src/main/java/com/imran/workmanage/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.imran.workmanage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val notificationWorkRequest: WorkRequest =
+            OneTimeWorkRequestBuilder<NotificationWorker>()
+                .build()
+
+
+        btn_notificaton.setOnClickListener(View.OnClickListener {
+            WorkManager
+                .getInstance(applicationContext)
+                .enqueue(notificationWorkRequest)
+        })
 
     }
 }
