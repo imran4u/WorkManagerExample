869a11373a90a63af02a7781b5fd6941ee218a66
imran <er.imran4u@gmail.com>
2020-06-24 16:09:30 +0530

    initial commit, with NotificationWroker class

---
+++ app/src/main/java/com/imran/workmanage/NotificationWorker.kt
@@ -0,0 +1,49 @@
+package com.imran.workmanage
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+/**
+ * Created by imran on 2020-06-24.
+ */
+class NotificationWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+        showNotification()
+        Result.Success
+    }
+
+    private fun showNotification() {
+        val notificationManager: NotificationManager =applicationContext.
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel("CHANNEL_ID", "name", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    description = "Channel description"
+                }
+                // Register the channel with the system
+
+                notificationManager.createNotificationChannel(channel)
+            }
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Test notification from title")
+            .setContentText("Test notification from description")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        // Set the intent that will
+        with(NotificationManagerCompat.from(applicationContext)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(12, builder.build())
+        }
+    }
+
+
+}
